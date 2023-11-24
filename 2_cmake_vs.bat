39228d75f4cd05d7a5c796f3b99a16a7b69a1c32
1vanK <1vanK@users.noreply.github.com>
2023-11-24 12:37:45 +0300

    Без system32 в PATH ссылки на папки не создаются

--- 2_cmake_vs.bat
+++ 2_cmake_vs.bat
@@ -1,11 +1,8 @@
 :: Меняем кодировку консоли на UTF-8
 chcp 65001
 
-:: При запуске батника от Админа меняется текущий путь. Фиксим это
-cd /D "%~dp0"
-
-:: Указываем путь к cmake.exe
-set "PATH=c:\programs\cmake\bin"
+:: Указываем путь к cmake.exe. Без system32 в PATH ссылки на папки не создаются 
+set "PATH=%SystemRoot%\system32;c:\programs\cmake\bin"
 
 :: Создаём проекты для Visual Studio 2022 в папке build_vs, используя конфиг CMakeLists.txt из папки game
 cmake.exe game -B build_vs -G "Visual Studio 17" -A x64
